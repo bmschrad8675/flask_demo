b2ac9cf966a579f69f583172cee72304a1ab09a6
Brandon Schrader <brandon@simpleltc.com>
2020-01-14 09:32:20 -0600

    Added a default endpoint

--- run.py
+++ run.py
@@ -3,6 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 # Routes
+@app.route('/')
+def home():
+    return """
+        <h1> Welcome </h1>
+        <div> You are authorized to view company information for id: 123 </div>
+        <div> The sample endpoint to view users list for the company is: '/users/123'
+        """
+
+
 @app.route('/users/<company_id>/')
 def userList(company_id):
     # Make sure they are authorized
